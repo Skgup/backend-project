88c13366b1b3baa8f96f7931c07be64d5286ebd9
Sumit Gupta <halwaisumit@gmail.com>
2025-05-09 15:54:34 +0530

    APIS handaling

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async ()=>{
+    try{
+        const connectionInstsnce =await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n Mongodb connected !! DB Host : ${connectionInstsnce}`)
+    }catch(err){
+        console.log("Mongodb connection error ", err);
+        process.exit(1)
+    }
+}
+
+export  default connectDB;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+import { app } from './app.js'
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8080 , ()=>{
+        console.log(`server is listening at  port ${process.env.PORT}`)
+    });
+})
+.catch((err)=>{
+    console.log("Mongo db connection failed !!!", err)
+})
+
